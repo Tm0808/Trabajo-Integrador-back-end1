7f13484e8f2e7217417f3083fd7ae829bc366890
tomas <tomasmartinolich33@gmail.com>
2024-03-26 22:42:56 -0300

    segundo commit

---
+++ src/main/java/com/example/TrabajointegradorbackendI/dto/request/TurnoRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.TrabajointegradorbackendI.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor @NoArgsConstructor
+public class TurnoRequestDTO {
+    private Long odontologo_id;
+    private Long paciente_id;
+    private String fecha;
+}
---
+++ src/main/java/com/example/TrabajointegradorbackendI/exception/BadRequestException.java
@@ -0,0 +1,2 @@
+package com.example.TrabajointegradorbackendI.exception;public class BadRequestException {
+}
